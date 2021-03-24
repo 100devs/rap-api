4ae965c7f918e7a30d4c906e1b8bd49f60f5405f
Leon Noel <leon.a.noel+github@gmail.com>
2021-03-23 18:54:19 -0700

    added port for heroku

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.get('/api/rappers/:rapperName', (request, response)=>{
     
 })
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
