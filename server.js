1a470b42cbac9ba5385084a0a9857edc642bf6db
Leon Noel <leon.a.noel+github@gmail.com>
2021-03-23 18:46:49 -0700

    working rap api

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+let rappers = {
+    '21 savage': {
+        'age': 28,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper':{
+        'age': 27,
+        'birthName': 'Chancelor Jonathan Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'dylan':{
+        'age': 28,
+        'birthName': 'dylan',
+        'birthLocation': 'dylan'
+    }
+} 
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/rappers/:rapperName', (request, response)=>{
+    const rapName = request.params.rapperName.toLowerCase()
+    console.log(rapName)
+    if(rappers[rapName]){
+        response.json(rappers[rapName])
+    }else{
+        response.json(rappers['dylan'])
+    }
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
